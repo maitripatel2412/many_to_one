91444a3d0ee2f7305577de6ab46d9477ba83411e
maitri patel <maitripatel2412@gmail.com>
2022-08-16 22:39:47 +0530

    second commit

--- many_to_one/src/main/java/in/example/many_to_one/controller/AccountController.java
+++ many_to_one/src/main/java/in/example/many_to_one/controller/AccountController.java
@@ -19,49 +19,44 @@ import in.example.many_to_one.service.AccountService;
 @RestController
 @RequestMapping("/accounts")
 public class AccountController {
-	
-	@Autowired
-	private AccountService aService;
 
-	@GetMapping("/hello")
-	public String account() {
-		return "hello its my account..   :)";
-	}
-	
-	@PostMapping("/save")
-	public Account createAccount(@RequestBody Account account) {
-		return aService.createAccount(account);
-	}
-	
-	@GetMapping
-	public List<Account> getAllAccounts(){
-		return aService.getAllAccounts();
-	}
-	
-	@GetMapping("/{id}")
-	public Account findById(@PathVariable Long id) {
-		return aService.findById(id);
-	}
-	
-	@PutMapping("/{id}")
-	public Account updateById(@PathVariable Long id,@RequestBody Account account) {
-		return aService.updateById(id,account);
-	}
-	
-	@DeleteMapping("/{id}")
-	public void deleteById(@PathVariable Long id) {
-		aService.deleteById(id);
-	}
-	
-	@GetMapping("/findByBranch/{id}")
-	public List<Account> findByBranchId(@PathVariable Long id) {
-		return aService.findByBranchId(id);
-	}
-	
-	@GetMapping("/BranchName")
-	public List<Account> findByBranchName(@RequestParam String name){
-		System.err.println("name : "+name);
-		return aService.findByBranchName(name);
-	}
-	
+    @Autowired
+    private AccountService aService;
+    @GetMapping("/hello")
+    public String account() {
+        return "hello its my account..   :)";
+    }
+
+    @PostMapping("/save")
+    public Account createAccount(@RequestBody Account account) {
+        return aService.createAccount(account);
+    }
+
+    @GetMapping
+    public List<Account> getAllAccounts() {
+        return aService.getAllAccounts();
+    }
+
+    @GetMapping("/{id}")
+    public Account findById(@PathVariable Long id) {
+        return aService.findById(id);
+    }
+
+    @PutMapping("/{id}")
+    public Account updateById(@PathVariable Long id, @RequestBody Account account) {
+        return aService.updateById(id, account);
+    }
+    @DeleteMapping("/{id}")
+    public void deleteById(@PathVariable Long id) {
+        aService.deleteById(id);
+    }
+    @GetMapping("/findByBranch/{id}")
+    public List<Account> findByBranchId(@PathVariable Long id) {
+        return aService.findByBranchId(id);
+    }
+    @GetMapping("/BranchName")
+    public List<Account> findByBranchName(@RequestParam String name) {
+        System.err.println("name : " + name);
+        return aService.findByBranchName(name);
+    }
 }
